5aed4f87a797d610fd6eb09ef37e502c3d6542b9
oederson <eder.hu@live.com.pt>
2024-01-21 04:11:26 -0300

    Primeiro c

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Sistema de Amortização Constante (SAC)");
+
+// Solicitar informações ao usuário
+Console.Write("Informe o número de parcelas: ");
+int numeroParcelas = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Informe o saldo devedor: ");
+double saldoDevedor = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe a taxa de juros (em %): ");
+double taxaJuros = Convert.ToDouble(Console.ReadLine());
+
+// Calcular a amortização constante
+double amortizacao = saldoDevedor / numeroParcelas;
+
+// Calcular o valor da parcela mensal
+double taxaJurosDecimal = taxaJuros / 100;
+double valorParcela = amortizacao + (saldoDevedor * taxaJurosDecimal);
+
+// Exibir os resultados
+Console.WriteLine("\nTabela de Amortização:");
+Console.WriteLine("Mês\tAmortização\tJuros\tSaldo Devedor\tParcela Mensal");
+
+for (int i = 1; i <= numeroParcelas; i++)
+{
+    double juros = saldoDevedor * taxaJurosDecimal;
+    saldoDevedor -= amortizacao;
+
+    Console.WriteLine($"{i}\t{amortizacao:F2}\t\t{juros:F2}\t{saldoDevedor:F2}\t\t{valorParcela:F2}");
+}
+
+Console.ReadLine();
